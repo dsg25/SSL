311b6020e53fa2d46da01051d031d2d4f7c5a7c9
Kocha <dsg25@bk.ru>
2022-12-13 01:32:20 -0800

    Working

--- SSL/Views/EQView.swift
+++ SSL/Views/EQView.swift
@@ -11,19 +11,36 @@ import SwiftUI
 struct EQView: View {
     var body: some View {
         ZStack {
-            
-                       
             Image("BG_EQ")
                 .resizable()
                 //.edgesIgnoringSafeArea(.top)
-                 .padding(-1)
-          
+                .padding(-1)
               
+            VStack {
                 
-    }
+                HStack {
+                    Image("Encoder_White")
+                       
+                    Image("Encoder_White")
+                        .offset(x:45, y:0)
+                }
+                    .offset(x:-80, y:-235)
+                
+                HStack {
+                    Image("Encoder_Red")
+                        
+                    Image("Encoder_Red")
+                      .offset(x:45, y:50)
+                }.offset(x:-80, y:-205)
+                
+                
+                
+            }
+            
+        }
         .background(Color.black)
         .edgesIgnoringSafeArea(.top)
-}
+    }
     
 }
 struct EQ_Previews: PreviewProvider {
