f37079c7287e68108d9b0af8faba55f84c62c6dd
Kocha <dsg25@bk.ru>
2022-12-12 22:03:17 -0800

    Initial Commit

---
+++ SSL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSL
+//
+//  Created by Kocha on 12.12.2022.
+//  Copyright © 2022 DarkGrp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
